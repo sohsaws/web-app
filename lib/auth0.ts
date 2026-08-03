220f731c991492c4799ef61aa9a0af9d65b0a90f
sohsaws <booleansd@proton.me>
2026-08-03 22:54:09 +0800

    Clerk and Auth0 sucks

--- lib/auth0.ts
+++ lib/auth0.ts
@@ -1,3 +1,3 @@
-export async function getAuth0Session() {
-  return null;
-}
+import { Auth0Client } from '@auth0/nextjs-auth0/server';
+
+export const auth0 = new Auth0Client();
--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,17 @@
-import { NextResponse, type NextRequest } from "next/server";
+import { auth0 } from "./lib/auth0";
 
-export default function proxy(request: NextRequest) {
-  return undefined;
+export async function proxy(request: Request) {
+  return await auth0.middleware(request);
 }
 
 export const config = {
-  matcher: ["/(dashboard|settings)(.*)", "/(api)(.*)", "/__clerk/(.*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
